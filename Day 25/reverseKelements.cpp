73e6cac468c1084da5470901ed1bd5b96275a22d
Santhosh <54148035+SanthoshM25@users.noreply.github.com>
2021-08-25 17:38:32 +0530

    Day 25 solution

---
+++ Day 25/reverseKelements.cpp
@@ -0,0 +1,19 @@
+queue<int> modifyQueue(queue<int> q, int k)
+{
+    //add code here.
+    queue<int> q2;
+    stack<int> stack;
+    for(int i = 0; i<k; i++){ 
+        stack.push(q.front());
+        q.pop();
+    }
+    while(!stack.empty()) {
+        q2.push(stack.top());
+        stack.pop();
+    }
+    while(q.size() > 0) {
+        q2.push(q.front());
+        q.pop();
+    }
+    return q2;
+}
